bd96fe26080b67c89bb82e8bba0e85e0e3f7c460
MickyC110181137 <1209436216micky@gmail.com>
2021-11-24 21:47:35 +0800

    unhash_11/24

--- NKUST/Hello_World1.java
+++ NKUST/Hello_World1.java
@@ -1,6 +1,19 @@
-public class Hello_World1 {
-    public static void main(String[] args)
-{
-    System.out.println("Hello World!");
-}
+class Hello_World1 {
+
+    public static void main (String[] args) {
+
+        System.out.println(hash("devgilno")); //574318821802
+    }
+
+    public static String letters = "cdefghijlmnoqstuvxz";
+
+
+
+    public static long hash(String s){
+        long h = 1;
+        for (int i = 0; i < s.length(); i++){
+            h = h * 23 + letters.indexOf(s.charAt(i));
+        }
+        return h;
+    }
 }
