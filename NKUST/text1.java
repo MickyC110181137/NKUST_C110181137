ddf544145b0621887ae137e5df6b9366883832a6
NKUST_C110181137 <1209436216micky@gmail.com>
2021-12-07 08:16:14 +0800

    text1

--- NKUST/text1.java
+++ NKUST/text1.java
@@ -1,14 +1,12 @@
-package out.production.NKUST;
-
 import java.util.HashSet;
 import java.util.ArrayList;
 
-public class text2 {
+public class text1 {
     public static void main(String[] args) {
         ArrayList<String> word1 = new ArrayList<String>();
         ArrayList<Character> word = new ArrayList<Character>();
         int b;
-        String[] num1 = {"0" , "0", "1" , "1" , "1" , "2" , "2" , "3" , "3" , "4" };
+        String[] num1 = { "1" , "1" , "2"  , "3"  };
 
         HashSet<String> num = new HashSet<String>();
         for(int d = 0; d < num1.length; d++){
