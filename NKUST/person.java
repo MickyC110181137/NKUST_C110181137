e3dc5b63ee53b1544261eb6f641be57455a40d45
kevin1547 <c110181143@nkust.edu.tw>
2021-12-16 17:37:55 +0800

    kevin1

--- NKUST/person.java
+++ NKUST/person.java
@@ -57,10 +57,6 @@ public class person {
         skillName = playerskill;
     }
 
-//    game(String med){
-//
-//    }
-
     void showplayer(){
         System.out.print(name);
         System.out.print("[");
@@ -76,35 +72,9 @@ public class person {
         this.player3 = player3;
     }
 
-//    int getHP() {
-//        return this.HP;
-//    }
-//
-//    int getAtt() {
-//        return this.Att;
-//    }
-//
-//    int getMP() {
-//        return this.MP;
-//    }
-
-//    String getSkillName() {
-//        return this.skillName;
-//    }
-
     String playername(){
         return this.name;
     }
-
-//    void showplayer1() {
-//        System.out.println(this.player1 + "[" + "HP:" + getHP() + "/" + "Att:" + getAtt() + "/" + "MP:" + getMP() + "]");
-//    }
-//    void showplayer2() {
-//        System.out.println(this.player2 + "[" + "HP:" + getHP() + "/" + "Att:" + getAtt() + "/" + "MP:" + getMP() + "]");
-//    }
-//    void showplayer3() {
-//        System.out.println(this.player3 + "[" + "HP:" + getHP() + "/" + "Att:" + getAtt() + "/" + "MP:" + getMP() + "]");
-//    }
 
     void player1shout() {
         System.out.println(this.name + "使出技能: " + skillName);
@@ -145,12 +115,6 @@ public class person {
     void recover() {
         System.out.println(this.name + "攻擊，能量不足，回復5點能量");
     }
-//    void recover2() {
-//        System.out.println(this.name + "攻擊，能量不足，回復5點能量");
-//    }
-//    void recover3() {
-//        System.out.println(this.name + "攻擊，能量不足，回復5點能量");
-//    }
 
     void playerAtt1(){
         player1shout();
@@ -182,9 +146,4 @@ public class person {
         attack13();
         //showplayer3();
     }
-
-
-
-
-
 }
