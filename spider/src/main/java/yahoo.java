d26b52acf611f0379e98c569f7e651d30cb976a4
NKUST_C110181137 <1209436216micky@gmail.com>
2022-01-03 23:57:45 +0800

    add kevin

--- spider/src/main/java/yahoo.java
+++ spider/src/main/java/yahoo.java
@@ -68,16 +68,6 @@ public class yahoo {
                         System.out.println("排名: " + r + " " + way1);
                     }
                 }
-                Document doc = Jsoup.connect("https://movies.yahoo.com.tw/chart.html").get();
-                System.out.println(doc.title());
-                String way = doc.select("#content_l > div > div.rank_list.table.rankstyle1 > div:nth-child(2) > div:nth-child(4) > a > dl > dd > h2").text();
-                System.out.println("排名: " + 1 + " " + way);
-                int r = 1;
-                for (int a = 3; a <= 21; a++ ){
-                    r = r + 1;
-                    String way1 = doc.select("#content_l > div > div.rank_list.table.rankstyle1 > div:nth-child(" + a + ") > div:nth-child(4) div").text();
-                    System.out.println("排名: " + r + " " + way1);
-                }
             }
             catch (Exception e) {
                 System.out.println("error: " + e);
