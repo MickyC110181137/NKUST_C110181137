67910627ccb6ecae623a5ac483e839438fee4cfb
Rufin <c110181111@nkust.edu.tw>
2022-01-04 23:10:09 +0800

    Rufin

--- spider/src/main/java/yahoo.java
+++ spider/src/main/java/yahoo.java
@@ -7,6 +7,22 @@ import java.util.SplittableRandom;
 
 public class yahoo {
     public static void main(String[] args) {
+        public static void main(String[] args) {
+            try {
+                for(int k = 1; k < 6; k++){
+                    int i = 0;
+                    Document doc = Jsoup.connect("https://movies.yahoo.com.tw/movie_intheaters.html?page=" + k).get();
+                    System.out.println(doc.title() + "Page" + k + "---------------------------------------------------------------------------------");
+                    for(int j = 1; j <= 10; j++){
+                        String name = doc.getElementsByClass("release_movie_name").get(i).text();
+                        i = i + 1;
+                        System.out.println("電影名稱: " + name);
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println("error: " + e);
+            }
+        }
             try {
                 for(int k = 1; k < 6; k++){//變換到不同頁數
                     int i = 0;
