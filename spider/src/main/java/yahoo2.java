ba9c990355d86d5007adb7f823fd1577f4b33e8c
MickyC110181137 <93572273+MickyC110181137@users.noreply.github.com>
2022-01-11 14:24:09 +0800

    Update yahoo2.java

--- spider/src/main/java/yahoo2.java
+++ spider/src/main/java/yahoo2.java
@@ -64,7 +64,7 @@ public class yahoo2 {
                     String way1 = doc.select("#content_l > div > div.rank_list.table.rankstyle1 > div:nth-child(" + a + ") > div:nth-child(4) div").text();//抓電影名稱
                     System.out.println("排名: " + r + " " + way1);
                 }
-            }//58到68都是我寫的~~一直被蓋掉，哭ㄚㄚㄚㄚ
+            }//58到68都是我寫的
         }catch (Exception e) {
             System.out.println("error: " + e);
         }
